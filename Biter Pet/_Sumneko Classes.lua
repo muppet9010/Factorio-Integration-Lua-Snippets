773bcb90561912ece324ccbcd0fe197fff953e98
Muppet9010 <mike.hollett@gmail.com>
2022-12-03 02:29:22 +0000

    Biter Pet - Initial status label work
    Added some extra status's, but more in the TODO comment.
    Not documented any of the new statusMessage tables as these are still in flux. The other new options I've added are documented.
    Made all of the variables in the function start with an underscore to help spot mistakes in scoping.

--- Biter Pet/_Sumneko Classes.lua
+++ Biter Pet/_Sumneko Classes.lua
@@ -1,11 +1,18 @@
 ---@class BiterPet_Data
----@field player LuaPlayer
----@field biter LuaEntity
----@field followPlayerFuncDump string
----@field followRange uint
----@field wonderMaxRange uint
----@field combatMaxRange uint
----@field calledBack boolean
----@field biterName string # Can be empty string for nil.
----@field hasOwner boolean
----@field debug boolean
+---@field _player LuaPlayer
+---@field _biter LuaEntity
+---@field _followPlayerFuncDump string
+---@field _closenessRange uint
+---@field _exploringMaxRange uint
+---@field _combatMaxRange uint
+---@field _calledBack boolean
+---@field _following boolean
+---@field _biterName string # Is either a name and hyphen with trailing space or empty string. Both are suitable for the biter state text to be added on to.
+---@field _hasOwner boolean
+---@field _debug boolean
+---@field _biterNameRenderId? uint64 # The render ID of the biters name and state text.
+---@field _biterStatusMessages_Wondering string[]
+---@field _biterStatusMessages_CallBack string[]
+---@field _biterStatusMessages_GuardingCorpse string[]
+---@field _biterStatusMessages_Dead string[]
+---@field _biterStatusMessages_Following string[]
