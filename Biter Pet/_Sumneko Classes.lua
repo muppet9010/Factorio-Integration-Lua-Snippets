403550e7ac48824370ac0282514b1a251c5c5544
Muppet9010 <mike.hollett@gmail.com>
2022-12-04 00:48:51 +0000

    Biter Pet - move state to own label
    Move state to own line for longer text.
    Add more varied initial state messages.

--- Biter Pet/_Sumneko Classes.lua
+++ Biter Pet/_Sumneko Classes.lua
@@ -7,12 +7,14 @@
 ---@field _combatMaxRange uint
 ---@field _calledBack boolean
 ---@field _following boolean
----@field _biterName string # Is either a name and hyphen with trailing space or empty string. Both are suitable for the biter state text to be added on to.
+---@field _biterName string # Is either a name or empty string.
 ---@field _hasOwner boolean
 ---@field _debug boolean
----@field _biterNameRenderId? uint64 # The render ID of the biters name and state text.
+---@field _biterNameRenderId? uint64 # The render ID of the biters name
+---@field _biterStateRenderId? uint64 # The render ID of the biters state text.
 ---@field _biterStatusMessages_Wondering string[]
 ---@field _biterStatusMessages_CallBack string[]
 ---@field _biterStatusMessages_GuardingCorpse string[]
 ---@field _biterStatusMessages_Dead string[]
 ---@field _biterStatusMessages_Following string[]
+---@field _biterStatusMessages_Fighting string[]
